777444b1e3d07fc8ec1c0b6e8d93cf86cd1825ec
Agim Coroli <agim.coroli8016@gmail.com>
2025-01-17 17:50:51 +0100

    tessst

--- view/MyView.php
+++ view/MyView.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
     <title><?=$datas['titre']?> | <?=$titrepage?></title>
 </head>
 <body>
@@ -16,21 +15,5 @@
     <h1><?=$datas['titre']?> | <?=$titrepage?></h1>
     <p><?=nl2br($text)?></p>
     <?php var_dump($_GET) ?>
-=======
-    <title><?php echo $datas['titre']?> | <?=$titrepage?></title>
-</head>
-<body>
-    <nav>
-        <a href="./">Accueil</a> 
-        <a href="./?p=php">PHP</a> 
-        <a href="./?p=sql">SQL</a>
-        <a href="./?p=javascript">Javascript</a>
-    </nav>
-    <h1><?=$datas['titre']?> | <?=$titrepage?></h1>
-    <p><?=nl2br($text)?></p>
-    <?php
-//var_dump($_GET);
-    ?>
->>>>>>> b8f7ec52f13db0939775d2ffafd103e4a11ed403
 </body>
 </html>
