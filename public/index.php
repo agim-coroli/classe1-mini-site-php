d833796114d879f3ab16f62ddaf9bc13affd7be4
Agim Coroli <agim.coroli8016@gmail.com>
2025-01-17 17:49:33 +0100

    test

--- public/index.php
+++ public/index.php
@@ -3,19 +3,6 @@
 
 // Front Controller
 
-<<<<<<< HEAD
-// chargement des données (les dependances)
-include "../datas/datas.php";
-
-// vérification si on a bien importer les données
-// var_dump($datas);
-
-// appel du rooter
-include "../controller/rooter.php";
-
-// Appel de la vue
-include "../view/MyView.php";
-=======
 // chargement des données (les dépendances)
 include "../datas/datas.php";
 
@@ -28,4 +15,3 @@ include "../controller/router.php";
 
 // Appel de la vue
 include "../view/MyView.php";
->>>>>>> b8f7ec52f13db0939775d2ffafd103e4a11ed403
