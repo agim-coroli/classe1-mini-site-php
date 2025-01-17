5c56f4e1c887a2ba3c686d590d1b7a8fc3f4a374
agim <agim.coroli8016@gmail.com>
2025-01-17 16:06:12 +0100

    test

--- public/index.php
+++ public/index.php
@@ -1,2 +1,14 @@
 <?php
 // Front Controller
+
+// chargement des données (les dependances)
+include "../datas/datas.php";
+
+// vérification si on a bien importer les données
+// var_dump($datas);
+
+// appel du rooter
+include "../controller/rooter.php";
+
+// Appel de la vue
+include "../view/MyView.php";
