b1f1fdf28cffb3486e3f60d7c19fc477cbd61fa3
agim <agim.coroli8016@gmail.com>
2025-01-22 16:21:03 +0100

    test

--- public/index.php
+++ public/index.php
@@ -5,6 +5,36 @@
 // des variables se trouvent dans l'URL
 // var_dump($_GET);
 
+// constante de la bdd
+const DB_MYSQL_TYPE = "mysql";
+const DB_MYSQL_HOST = "localhost";
+const DB_MYSQL_NAME = "db_mini_site";
+const DB_MYSQL_LOGIN = "root";
+const DB_MYSQL_PWD = "";
+const DB_MYSQL_PORT =  3306;
+const DB_MYSQL_ENCODE = "utf8";
+
+// connexion a la base de donnée
+try {
+    $connectBD = new PDO(DB_MYSQL_TYPE.":host=".DB_MYSQL_HOST.";dbname=".DB_MYSQL_NAME.";port".DB_MYSQL_PORT.";charset=".DB_MYSQL_ENCODE, 
+    DB_MYSQL_LOGIN, 
+    DB_MYSQL_PWD
+);
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+// verification de la variable get requestGET pour effectuer la requete SQL
+if(isset($_GET['requestGET'])) {
+    $sql = "SELECT * FROM `pages` WHERE `id` = 2;";
+} else{
+    $sql = "SELECT * FROM `pages` WHERE `id` = 1;";
+}
+
+//  EXecution de la requete
+$recup = $connectBD->query($sql);
+//  Transformation du resultat en format phph tableau associatif
+$response = $recup->fetch(PDO::FETCH_ASSOC);
 
 // On appel la vue
 ?>
@@ -22,8 +52,8 @@
         <a href="./?requestGET=sql">SQL</a>
         <a href="./?requestGET=javascript">javascript</a>
     </nav>
-    <h1></h1>
-    <p></p>
-    <?php var_dump($_GET) ?>
+    <h1><?php echo $response['titlep'] ?></h1>
+    <p><?php echo $response['textp'] ?></p>
+    <?php var_dump($_GET,$connectBD,) ?>
 </body>
 </html>
